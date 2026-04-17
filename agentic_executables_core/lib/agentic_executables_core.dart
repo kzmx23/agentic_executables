146ca14b5357b238cef2f1ea51ff99b98590aa4d
arenukvern <anton@xsoulspace.dev>
2026-04-17 04:44:07 +0300

    feat(core): add ArtifactStore port

--- agentic_executables_core/lib/agentic_executables_core.dart
+++ agentic_executables_core/lib/agentic_executables_core.dart
@@ -22,6 +22,7 @@ export 'src/models/registry.dart';
 export 'src/models/requires_spec.dart';
 export 'src/models/types.dart';
 export 'src/models/verify.dart';
+export 'src/ports/artifact_store.dart';
 export 'src/ports/canonical_store.dart';
 export 'src/ports/document_store.dart';
 export 'src/ports/generation_engine.dart';
