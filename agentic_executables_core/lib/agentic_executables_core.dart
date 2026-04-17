033276936cc2dd3c1befdd8ecfb4711394d68a17
arenukvern <anton@xsoulspace.dev>
2026-04-17 04:31:18 +0300

    feat(core): add ArtifactMatrix model

--- agentic_executables_core/lib/agentic_executables_core.dart
+++ agentic_executables_core/lib/agentic_executables_core.dart
@@ -3,6 +3,7 @@ library;
 export 'src/config/ae_core_config.dart';
 export 'src/models/ae_error.dart';
 export 'src/models/ae_result.dart';
+export 'src/models/artifact_matrix.dart';
 export 'src/models/canonical_matrix.dart';
 export 'src/models/canonical_pack.dart';
 export 'src/models/evaluate.dart';
